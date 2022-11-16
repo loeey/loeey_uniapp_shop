6af0af289e938661e3fb03fda04bd76d604902a3
舒博凯 <853313515@qq.com>
2022-11-16 22:02:38 +0800

    完成了 home 首页的开发

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 import App from './App'
 
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
